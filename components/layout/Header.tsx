955ea1c3746993563a8104af8d7462f52ba17531
Azrul Haque <azrul.haque@netsmartz.local>
2026-04-10 15:04:28 +0530

    header fixes 1

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -246,15 +246,15 @@ const Header: React.FC = () => {
         { 
           title: '', 
           links: [
-            { label: 'Microsoft Partner', href: '/partners/microsoft', icon: '/assets/products/microsoft.webp' },
-            { label: 'IBM Partner', href: '/partners/ibm', icon: '/assets/products/ibm.webp' },
-            { label: 'Adobe Partner', href: '/partners/adobe', icon: '/assets/products/adobe.webp' },
-            { label: 'Salesforce Partner', href: '/partners/salesforce', icon: '/assets/products/salesforce.webp' },
-            { label: 'ServiceNow Partner', href: '/partners/servicenow', icon: '/assets/products/servicenow.webp' },
-            { label: 'AWS Partner', href: '/partners/aws', icon: '/assets/products/aws.webp' },
-            { label: 'Google Partner', href: '/partners/google', icon: '/assets/products/google.webp' },
-            { label: 'Crowdstrike Partner', href: '/partners/crowdstrike', icon: '/assets/products/crowdstrike.webp' },
-            { label: 'SentinelOne Partner', href: '/partners/sentinelone', icon: '/assets/products/sentinel.webp' }
+            { label: 'Microsoft Partner', href: '/partners/microsoft', icon: '/assets/partners/microsoft.webp' },
+            { label: 'IBM Partner', href: '/partners/ibm', icon: '/assets/partners/ibm.webp' },
+            { label: 'Adobe Partner', href: '/partners/adobe', icon: '/assets/partners/adobe.webp' },
+            { label: 'Salesforce Partner', href: '/partners/salesforce', icon: '/assets/partners/salesforce.webp' },
+            { label: 'ServiceNow Partner', href: '/partners/servicenow', icon: '/assets/partners/servicenow.webp' },
+            { label: 'AWS Partner', href: '/partners/aws', icon: '/assets/partners/aws.webp' },
+            { label: 'Google Partner', href: '/partners/google', icon: '/assets/partners/google.webp' },
+            { label: 'Crowdstrike Partner', href: '/partners/crowdstrike', icon: '/assets/partners/crowdstrike.webp' },
+            { label: 'SentinelOne Partner', href: '/partners/sentinelone', icon: '/assets/partners/sentinel.webp' }
           ] 
         }
       ]
